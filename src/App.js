e1425eadb017c1e96c7aefc6b7654e00bd1da20f
abhishek-thakur09 <abburajput7876@gmail.com>
2025-06-17 08:38:26 +0530

    created a server

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.send("Namste");
+})
+
+app.use('/text', (req, res)=>{
+    res.send("Namste!!");
+})
+
+app.listen(9999, ()=>{
+    console.log(`App listening on port 9999`);
+})
