031929d50094bb0cffb5da3696a36bd32425da1f
Uvissh <vishalbhateria@gmail.com>
2025-06-17 10:04:31 +0530

    Your descriptive commit message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send("Namste");
+    res.send("Namste london");
 })
 
 app.use('/text', (req, res)=>{
